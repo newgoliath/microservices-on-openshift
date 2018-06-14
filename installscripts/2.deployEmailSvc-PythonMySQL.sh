5e0f1a5769279ca27a5a057fc8fdb24d2aafad54
Jamie Pollard <jpollard@redhat.com>
2018-06-14 07:56:06 +0000

    Separated mysql pod definition out into mysql.yaml file containing updated DeploymentConfig - this prevents a race condition which can prevent the MySQL schema from being correctly deployed. Also included sample INSERT statements as post-deployment hook to populate the database.

--- installscripts/2.deployEmailSvc-PythonMySQL.sh
+++ installscripts/2.deployEmailSvc-PythonMySQL.sh
@@ -1,16 +1,5 @@
 oc project $OSE_INFRA_PROJECT
-oc new-app -e MYSQL_USER='app_user' \
-MYSQL_PASSWORD='password' \
-MYSQL_DATABASE=microservices \
- registry.access.redhat.com/rhscl/mysql-56-rhel7 --name='mysql' -l microservice=emailsvc
-
-sleep 10
-
-oc rollout cancel mysql 
-
-oc patch dc/mysql --patch '{"spec":{"strategy":{"rollingParams":{"post":{"failurePolicy": "ignore","execNewPod":{"containerName":"mysql","command":["/bin/sh","-c","hostname&&sleep 10&&echo $QUERY | /opt/rh/rh-mysql56/root/usr/bin/mysql -h $MYSQL_SERVICE_HOST -u $MYSQL_USER -D $MYSQL_DATABASE -p$MYSQL_PASSWORD -P 3306"], "env": [{"name": "QUERY", "value":"CREATE TABLE IF NOT EXISTS emails \u0028from_add varchar\u002840\u0029,to_add varchar\u002840\u0029, subject varchar\u002840\u0029, body varchar\u0028200\u0029, created_at date\u0029;"}]}}}}}}'
-
-oc rollout latest mysql
+oc create -f ./mysql.yaml
 
 oc new-app --context-dir='python-email-api' \
   -e EMAIL_APPLICATION_DOMAIN=http://emailsvc:8080 \
